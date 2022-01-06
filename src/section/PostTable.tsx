a1afda8d6ff83b5f6dbd58d004e74987a23722ec
Ramajedd <40004459+ramajedd@users.noreply.github.com>
2022-01-06 09:54:31 -0800

    update post list table

--- src/section/PostTable.tsx
+++ src/section/PostTable.tsx
@@ -1,6 +1,8 @@
 import { SetStateAction, useState } from 'react';
 import { sentenceCase } from 'change-case';
 // @mui
+import Image from 'src/components/Image';
+import { styled } from '@mui/material/styles';
 import { useTheme } from '@mui/material/styles';
 import {
   Box,
@@ -16,6 +18,8 @@ import {
   CardHeader,
   IconButton,
   TableContainer,
+  Typography,
+  Stack
 } from '@mui/material';
 // utils
 // import { fCurrency } from 'src/utils/formatNumber';
@@ -28,56 +32,93 @@ import Scrollbar from 'src/components/Scrollbar';
 import MenuPopover from 'src/components/MenuPopover';
 
 const _appInvoices = [...Array(5)].map((_, index) => ({
-    id: `${Date.now() + index}`,
-    price: 2000,
-    // category: randomInArray(['Android', 'Mac', 'Windows']),
-    // status: randomInArray(['paid', 'out_of_date', 'in_progress']),
-    category: 'Android',
-    status: 'paid',
-  }));
+  id: `${Date.now() + index}`,
+  title: 'Diisi judul postingan',
+  author: 'Mahmud',
+  category: "belajar",
+  // category: randomInArray(['Android', 'Mac', 'Windows']),
+  // status: randomInArray(['paid', 'out_of_date', 'in_progress']),
+  tag: 'matematika',
+  status: 2 ? 'terbit' : 'draft',
+}));
 // ----------------------------------------------------------------------
+
+const ItemBlockStyle = styled((props) => <Stack direction="row" alignItems="center" {...props} />)({
+  minWidth: 72,
+  flex: '1 1',
+});
+
+const ItemIconStyle = styled(Iconify)(({ theme }) => ({
+  width: 16,
+  height: 16,
+  marginRight: theme.spacing(0.5),
+  color: theme.palette.text.disabled,
+}));
 
 export default function PostTable() {
   const theme = useTheme();
 
   return (
     <Card>
-      <CardHeader title="New Invoice" sx={{ mb: 3 }} />
+      <CardHeader title="Post" sx={{ mb: 3 }} />
       <Scrollbar sx={undefined}>
         <TableContainer sx={{ minWidth: 720 }}>
           <Table>
             <TableHead>
               <TableRow>
-                <TableCell>Invoice ID</TableCell>
-                <TableCell>Category</TableCell>
-                <TableCell>Price</TableCell>
+                <TableCell>Judul</TableCell>
+                <TableCell>Penulis</TableCell>
+                <TableCell>Kategori</TableCell>
+                <TableCell>Tag</TableCell>
                 <TableCell>Status</TableCell>
                 <TableCell />
               </TableRow>
             </TableHead>
             <TableBody>
               {_appInvoices.map((row) => (
+                <>
                 <TableRow key={row.id}>
-                  <TableCell>{`INV-${row.id}`}</TableCell>
+                  {/* <TableCell>{`INV-${row.id}`}</TableCell> */}
+                  <TableCell>
+                    <ItemBlockStyle sx={{ minWidth: 120 }}>
+                      <Image disabledEffect ratio={undefined} alt={'amir'} src='https://minimal-assets-api.vercel.app/assets/icons/ic_flag_us.svg' sx={{ width: 28, mr: 1 }} />
+                      <Typography variant="subtitle2">{row.title}</Typography>
+                    </ItemBlockStyle>
+                  </TableCell>
+                  <TableCell>{row.author}</TableCell>
                   <TableCell>{row.category}</TableCell>
                   {/* <TableCell>{fCurrency(row.price)}</TableCell> */}
-                  <TableCell>{row.price}</TableCell>
+                  <TableCell>{row.tag}</TableCell>
                   <TableCell>
                     <Label
                       variant={theme.palette.mode === 'light' ? 'ghost' : 'filled'}
                       color={
-                        (row.status === 'in_progress' && 'warning') ||
-                        (row.status === 'out_of_date' && 'error') ||
+                        (row.status === 'draft' && 'warning') ||
+                        (row.status === 'undefined' && 'error') ||
                         'success'
                       }
                     >
                       {sentenceCase(row.status)}
                     </Label>
                   </TableCell>
+                  
                   <TableCell align="right">
                     <MoreMenuButton />
                   </TableCell>
                 </TableRow>
+                  <Box sx={{ margin: 1 }}>
+                    <Table size="small" aria-label="purchases">
+                      <TableHead>
+                        <TableRow>
+                          <TableCell>Date</TableCell>
+                          <TableCell>Customer</TableCell>
+                          <TableCell align="right">Amount</TableCell>
+                          <TableCell align="right">Total price ($)</TableCell>
+                        </TableRow>
+                      </TableHead>
+                    </Table>
+                  </Box>
+              </>
               ))}
             </TableBody>
           </Table>
